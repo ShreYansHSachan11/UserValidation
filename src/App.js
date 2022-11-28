a23df29eed3445959a1c719dff82418fcc901574
ShreYansHSachan11 <shreyanshsachan2016@gmail.com>
2022-11-28 22:02:21 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUser, faKey , faEye} from '@fortawesome/free-solid-svg-icons'
 
-function App() {
+import {BrowserRouter as Router, Routes, Route, NavLink} from "react-router-dom";
+import './App.css'
+import Login from './Login'
+import Register from './Register'
+import Verified from './Verified'
+import ResetPassword from './ResetPassword'
+import MainPage from './MainPage'
+import Otp from './Otp'
+import Recaptcha from './components/recaptcha/Recaptcha'
+
+library.add(faUser, faKey , faEye)
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      
+        
+        
+        <NavLink to="/Verified"></NavLink>
+        
+        <NavLink to="/MainPage"></NavLink>
+
+        <Routes>
+        <Route exact path="/" element={<Login/>}/>
+        <Route exact path="/register" element={<Register/>}/>
+        <Route exact path="/verified" element={<Verified/>}/>
+        <Route exact path="/resetpassword" element={<ResetPassword/>}/>
+        <Route exact path="/mainpage" element={<MainPage/>}/>
+        <Route exact path="/otp" element={<Otp/>}/>
+        <Route exact path="/recaptcha" element={<Recaptcha/>}/>
+        
+        
+       
+      </Routes>
+    </Router>
+  )
+  
 }
 
-export default App;
+export default App
